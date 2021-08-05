43679a31c40e608ca35437c1cec4b1344ffe7770
Aadil Ghani <aadilghani1@gmail.com>
2021-08-06 03:47:11 +0530

    1st iteration

---
+++ screens/EatsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+import tw from "tailwind-react-native-classnames";
+const EatsScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text style={tw`text-red-500`}>This page is under construction</Text>
+    </SafeAreaView>
+  );
+};
+
+export default EatsScreen;
+
+const styles = StyleSheet.create({});
